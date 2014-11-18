568d01bdb8d3830f6fa0b72908dec05f08e4a972
ryujiin <ryujiin22@gmail.com>
2014-11-18 09:16:29 -0500

    facebook login

--- cliente/views.py
+++ cliente/views.py
@@ -10,13 +10,16 @@ from rest_framework import authentication, permissions, parsers, renderers
 from rest_framework import viewsets, generics
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.decorators import api_view, throttle_classes
-from social.apps.django_app.utils import strategy
+from social.apps.django_app.utils import psa
 
 from django.http import HttpResponse, Http404
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
+
+from django.contrib.auth import login
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
 
 
@@ -40,6 +43,15 @@ class UsuarioViewSet(viewsets.ReadOnlyModelViewSet):
 	serializer_class = UsuarioSerializer
 
 
+@csrf_exempt
+@psa('social:complete')
+def register_by_access_token(request, backend):
+	token = request.GET.get('access_token')
+	backend = request.backend
+	user = backend.do_auth(token)
+	return user
+
+
 class ObtainAuthToken(APIView):
 	throttle_classes = ()
 	permission_classes = ()
@@ -52,26 +64,13 @@ class ObtainAuthToken(APIView):
 	def post(self, request, backend):
 		serializer = self.serializer_class(data=request.DATA)
 
-		if backend == 'auth':
-			if serializer.is_valid():
-				token, created = Token.objects.get_or_create(user=serializer.object['user'])
-				return Response({'token': token.key})
-			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-		else:
 			# Here we call PSA to authenticate like we would if we used PSA on server side.
-			user = register_by_access_token(request, backend)
+		user = register_by_access_token(request, backend)
+		if user:
+			login(request, user)
 
-			# If user is active we get or create the REST token and send it back with user data
-			if user and user.is_active:
-				token, created = Token.objects.get_or_create(user=user)
-				return Response({'id': user.id , 'name': user.username, 'userRole': 'user','token': token.key})
+		# If user is active we get or create the REST token and send it back with user data
+		if user and user.is_active:
+			token, created = Token.objects.get_or_create(user=user)
+			return Response({'id': user.id , 'name': user.username, 'userRole': 'user','token': token.key})
 
-@strategy()
-def register_by_access_token(request, backend):
-	backend = request.strategy.backend
-	user = request.user
-	user = backend._do_auth(
-		access_token=request.GET.get('access_token'),
-		user=user.is_authenticated() and user or None
-	)
-	return user
